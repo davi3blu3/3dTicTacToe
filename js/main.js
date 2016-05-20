5b3f97870c554dab438adaeb011eca14f9add323
Dave Harned <dharned@gmail.com>
2016-05-20 17:47:57 -0500

    little tweaks and comments added

--- js/main.js
+++ js/main.js
@@ -1,30 +1,37 @@
 $(document).ready(function() {
+    var turn = true, // alternates between click animation
+        i;
     
     $('.cascade-1').afterTime(2000, function () {
         $('.cascade-1').addClass("clearBlock");
     });
-    $('.cascade-2').afterTime(2200, function () {
+    $('.cascade-2').afterTime(2250, function () {
         $('.cascade-2').addClass("clearBlock");
     });
-    $('.cascade-3').afterTime(2400, function () {
+    $('.cascade-3').afterTime(2500, function () {
         $('.cascade-3').addClass("clearBlock");
     });
-    $('.cascade-4').afterTime(2600, function () {
+    $('.cascade-4').afterTime(2750, function () {
         $('.cascade-4').addClass("clearBlock");
     });
-    $('.cascade-5').afterTime(2800, function () {
+    $('.cascade-5').afterTime(3000, function () {
         $('.cascade-5').addClass("clearBlock");
     });
-    $('.cascade-6').afterTime(3000, function () {
+    $('.cascade-6').afterTime(3250, function () {
         $('.cascade-6').addClass("clearBlock");
     });
 
     
+    
     $('.box').click(function() {
         $(this).removeClass('clearBlock');
-        // $(this).addClass('showRight');
-        $(this).addClass('showLeft');
-       
+        if(turn) {
+            $(this).addClass('showRight');
+            turn = false;
+        } else {
+            $(this).addClass('showLeft');
+            turn = true;
+        }
     });
 
 });
