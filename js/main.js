8fc166ac651102a73297e539274913cbba431b19
Dave Harned <dharned@gmail.com>
2016-05-20 09:45:45 -0500

    testing cascading animation

--- js/main.js
+++ js/main.js
@@ -2,7 +2,21 @@ $(document).ready(function() {
     
     $('.cascade-1').afterTime(2000, function () {
         $('.cascade-1').addClass("clearBlock");
-        alert($('.cascade-1'));
+    });
+    $('.cascade-2').afterTime(2200, function () {
+        $('.cascade-2').addClass("clearBlock");
+    });
+    $('.cascade-3').afterTime(2400, function () {
+        $('.cascade-3').addClass("clearBlock");
+    });
+    $('.cascade-4').afterTime(2600, function () {
+        $('.cascade-4').addClass("clearBlock");
+    });
+    $('.cascade-5').afterTime(2800, function () {
+        $('.cascade-5').addClass("clearBlock");
+    });
+    $('.cascade-6').afterTime(3000, function () {
+        $('.cascade-6').addClass("clearBlock");
     });
     
 });
